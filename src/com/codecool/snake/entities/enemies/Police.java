a2218964ea50ef286c5c7831456b138cdbf8ca1c
BalintAts <atsbalint@gmail.com>
2019-08-07 21:21:44 +0100

    police rotating not well

--- src/com/codecool/snake/entities/enemies/Police.java
+++ src/com/codecool/snake/entities/enemies/Police.java
@@ -9,6 +9,7 @@ import com.codecool.snake.entities.snakes.Snake;
 import com.codecool.snake.entities.snakes.SnakeHead;
 import com.sun.javafx.geom.Vec2d;
 import javafx.geometry.Point2D;
+import javafx.scene.transform.Rotate;
 
 import java.util.List;
 import java.util.Random;
@@ -23,70 +24,37 @@ public class Police extends Enemy implements Animatable, Interactable {
         super(10);
         boolean canSpwan = canSpawn();
         System.out.println("I just spawned");
-//        if (!canSpwan)
-//            Globals.getInstance().game.spawnEnemies(1);
-//            destroy();
-//        }
-
+        if (!canSpwan){
+            Globals.getInstance().game.spawnPolice();
+            destroy();
+        }
         setImage(Globals.getInstance().getImage("SnakeHead"));
-//        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
-//        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
-
-        setX(300);
-        setY(300);
-
+        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
+        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
 
         double direction = rnd.nextDouble() * 360;
         setRotate(direction);
 
         int speed = 0;
         heading = Utils.directionToVector(direction, speed);
-
-
-
-
     }
 
 
     @Override
     public void step() {
-        double x = 0;
-        double y = 0;
-        Vector snake1Pos;
-        double snake1posX = 0;
-        double snake1posY = 0;
+        double snake1posX;
+        double snake1posY;
         snake1posX = Globals.getInstance().snakes.get(0).getHead().getPosition().x;
         snake1posY = Globals.getInstance().snakes.get(0).getHead().getPosition().y;
-//        snake1Pos = Globals.getInstance().snakes.get(0).getHead().getPosition();
-        double angle = 0;
+        double angle;
 //        angle = Math.toDegrees(Math.atan((this.getY()-snake1posY)/(this.getX()-snake1posX)));
-        angle = Math.toDegrees(Math.atan(snake1posY)/(snake1posX));
+        angle = Math.toDegrees(Math.atan(snake1posY - getY())/(snake1posX - getX()));;
         heading = Utils.directionToVector(angle, 1);
-
-//        heading = new Point2D(getX() - snake1posX, getY() - snake1posY);
-//        if (isOutOfBounds()) {
-//            System.out.println("I am just destroyed");
-//            destroy();
-//        }
-        System.out.println("x  " + getX());
-        System.out.println("y  " + getY());
-//        setX(getX() + heading.getX());
-//        setY(getY() + heading.getY());
-//        double vectorMag = Math.sqrt((getX() - snake1posX)*(getX() - snake1posX) + (getY() - snake1posY )*(getY() - snake1posY ));
-//        setX(getX() + ((getX() - snake1posX)/vectorMag));
-//        setY(getY() + ((getY() - snake1posY)/vectorMag));
-
+        System.out.println(angle);
         double vectorMag = Math.sqrt((snake1posX - getX())*(snake1posX - getX()) +  (snake1posY - getY())*(snake1posY - getY()));
         setX(getX() + (snake1posX - getX())/vectorMag);
         setY(getY() + (snake1posY - getY())/vectorMag);
-
-;
-
-//        System.out.println((getX() + (getX() - snake1posX)/vectorMag));
-
-
-//        double moveToX = snake1posX - getX();
-//        double mo
+        setRotate(angle);
     }
 
 
@@ -94,9 +62,8 @@ public class Police extends Enemy implements Animatable, Interactable {
     public void apply(GameEntity entity) {
         if (entity instanceof SnakeHead) {
             System.out.println(getMessage());
-            Globals.getInstance().game.spawnEnemies(1);
+            Globals.getInstance().game.spawnPolice();
             destroy();
-
         }
     }
 
@@ -104,8 +71,6 @@ public class Police extends Enemy implements Animatable, Interactable {
     public String getMessage() {
         return (getDamage() + " damage");
     }
-
-
 }
 
 
